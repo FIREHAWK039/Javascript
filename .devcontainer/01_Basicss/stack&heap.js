bf66d30a580d0a6611224c05adb320e3ce7161df
FIREHAWK039 <FIREHAWK039@GMAIL.COM>
2025-10-01 20:40:04 +0000

    stackheap

---
+++ .devcontainer/01_Basicss/stack&heap.js
@@ -0,0 +1,22 @@
+// stack and heap 
+let myyoutubename = "class 100 csbe"
+
+let anothername = myyoutubename
+anothername = "chai sutta"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+// ----------------------------------
+
+let user1 ={
+    email: "usera=mail.com",
+    name: "us3er1"
+}
+
+let user2= user1
+
+user2.email = "userupadted*mail.com"
+ console.log(user1.email);
+ console.log(user2.email);
+ 
