369dce32b074dd9a92299e05e5d62957593c22c4
FIREHAWK039 <FIREHAWK039@GMAIL.COM>
2025-10-01 19:28:37 +0000

    comparision4

--- .devcontainer/01_Basicss/datatypesss.js
+++ .devcontainer/01_Basicss/datatypesss.js
@@ -35,7 +35,7 @@ let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
-// console.log(2-2);
+// console.log(2-2); 
 // console.log(2*2);
 // console.log(2**3);
 // console.log(2/3);
