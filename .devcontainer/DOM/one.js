37091688ea9d88617f6ffc96da020fbfa837245e
FIREHAWK039 <FIREHAWK039@GMAIL.COM>
2025-10-12 10:06:13 +0000

    js

---
+++ .devcontainer/DOM/one.js
@@ -0,0 +1,8 @@
+console.log(window)
+console.log(window.document)
+console.dir(document);
+// console.log(document.xyz);
+
+
+document.getElementById('firstheadinng')
+document.getElementById('firstheadinng').innerHTML = "chai aur code"
